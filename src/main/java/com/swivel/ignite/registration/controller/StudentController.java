17f4a7eee1918e625df0a3ca4ef392d628e4e1ef
Nawaz-swivel <nawas@swivelgroup.com.au>
2023-03-13 09:52:22 +0530

    added cross origin

--- src/main/java/com/swivel/ignite/registration/controller/StudentController.java
+++ src/main/java/com/swivel/ignite/registration/controller/StudentController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/v1/student")
 @Slf4j
+@CrossOrigin
 public class StudentController extends Controller {
 
     private final StudentService studentService;
